e66f8b9bb724bd5b1dc8ca58b13182854b1fe1f1
Office Laptop <sebghatullah1994@gmail.com>
2025-01-06 18:17:20 +0430

    updated

--- dist/script.js
+++ dist/script.js
@@ -2,44 +2,40 @@ const inputForm = document.getElementById("inputForm");
 const tableBody = document.getElementById("tableBody");
 const SaveBtn = document.getElementById("SaveBtn");
 
-const Collection = [];
+const customers = [];
+SaveBtn.addEventListener("click", store);
 
-SaveBtn.addEventListener("click", Sherower);
-
-function Sherower() {
-  const name = document.getElementById("name").value;
-  const lastName = document.getElementById("lastName").value;
-  const age = document.getElementById("age").value;
-  //   console.log(name, lastName, age);
-  Collection.push({ name, lastName, age });
-}
-
-tableBody.addEventListener("beforeend", renderTable);
-
-function renderTable() {
-  rows = `  <tr class="border w-full">
-            <td class="border w-52">#.01</td>
-            <td class="border w-52">Sebghat</td>
-            <td class="border w-52">Amin</td>
-            <td class="border w-52">30</td>
+/**
+ * @description function index used for generate rows on table based on cu
+ * customers array
+ */
+function index() {
+  tableBody.innerHTML = ""; // delete all rows before iteration
+  customers.forEach(function listing(details, idx) {
+    console.log(details);
+    tableBody.innerHTML += `<tr class="border w-full">
+            <td class="border w-52">${idx + 1}</td>
+            <td class="border w-52">${details.name}</td>
+            <td class="border w-52">${details.lastName}</td>
+            <td class="border w-52">${details.age}</td>
             <td class="border w-52">
-              <button class="bg-neutral-600">Edit</button>
+              <button class="bg-neutral-600" onclick="edit(${idx})">Edit</button>
               <button class="bg-neutral-600">Delete</button>
             </td>
           </tr>`;
+    // tableBody.insertAdjacentHTML("beforeend", rows);
+  });
 }
+function store() {
+  const fname = document.getElementById("name").value;
+  const lastName = document.getElementById("lastName").value;
+  const age = document.getElementById("age").value;
+  customers.push({ name: fname, lastName, age });
+  console.log(customers);
 
-// function Test() {
-//   SaveBtn.addEventListener("click", () => {
-//     const row = inputForm.querySelectorAll("input").value;
-//     const col = row.Collection.map(function (val) {
-//       return {
-//         row: val,
-//         row: val,
-//         row: val,
-//       };
-//     });
-
-//     console.log(col);
-//   });
-// }
+  // inputForm.reset();
+  index();
+}
+function edit(index) {
+  console.log("first", index);
+}
