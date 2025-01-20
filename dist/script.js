b6d0c9731b138baa44c745ea33df692b1912100a
Office Laptop <sebghatullah1994@gmail.com>
2025-01-20 14:54:53 +0430

    updated

--- dist/script.js
+++ dist/script.js
@@ -12,42 +12,52 @@ function dataStore() {
   const bookName = document.getElementById("name").value;
   const author = document.getElementById("author").value;
   const price = document.getElementById("price").value;
-  library.push({ bookName, author, price });
 
   if (!bookName || !author || !price) {
-    alert("please fill out the form");
-  } else {
-    renderTable();
+    alert("Please fill out the form");
+    return;
   }
+
+  if (globalIndex > -1) {
+    library[globalIndex] = { bookName, author, price };
+    globalIndex = -1;
+    saveBtn.textContent = "Save";
+  } else {
+    library.push({ bookName, author, price });
+  }
+
+  inputForm.reset();
+  renderTable();
 }
+
 function renderTable() {
   dataTable.innerHTML = "";
-  // console.log(library);
-  library.forEach(function rowPrint(itm, idx) {
+
+  library.forEach((item, idx) => {
     const rows = `<tr class="w-full rounded border">
-  <td class="w-52 rounded-md border">${idx + 1}</td>
-  <td class="w-52 rounded-md border">${itm.bookName}</td>
-  <td class="w-52 rounded-md border">${itm.author}</td>
-  <td class="w-52 rounded-md border">${itm.price}</td>
-  <td class="w-52 rounded-md border">
-  <button class="bg-orange-500 rounded" onClick="edit('${idx}')">Edit</button>
-  <button class="bg-red-600 rounded" onClick="remove()">Delete</button>
-  </td>
-</tr>`;
+      <td class="w-52 rounded-md border">${idx + 1}</td>
+      <td class="w-52 rounded-md border">${item.bookName}</td>
+      <td class="w-52 rounded-md border">${item.author}</td>
+      <td class="w-52 rounded-md border">${item.price}</td>
+      <td class="w-52 rounded-md border">
+        <button class="bg-orange-500 rounded" onClick="edit(${idx})">Edit</button>
+        <button class="bg-red-600 rounded" onClick="remove(${idx})">Delete</button>
+      </td>
+    </tr>`;
     dataTable.insertAdjacentHTML("beforeend", rows);
   });
 }
 
-function remove() {
-  library.splice(0, 1);
+function remove(idx) {
+  library.splice(idx, 1);
   renderTable();
 }
 
-function edit(id) {
-  let selectedRow = library[id];
+function edit(idx) {
+  const selectedRow = library[idx];
   document.getElementById("name").value = selectedRow.bookName;
   document.getElementById("author").value = selectedRow.author;
   document.getElementById("price").value = selectedRow.price;
-  renderTable();
-  //I don't know what to do
+  globalIndex = idx;
+  saveBtn.textContent = "Update";
 }
