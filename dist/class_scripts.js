588353adac4ef0ef79ac83c24b3e411ae01517a0
Office Laptop <sebghatullah1994@gmail.com>
2025-01-25 14:40:35 +0430

    update

--- dist/class_scripts.js
+++ dist/class_scripts.js
@@ -9,18 +9,18 @@ class Library {
     this.inputForm = document.querySelector("#inputForm");
     this.update_record_index = -1;
     this.saveBtn = document.getElementById("saveBtn");
+    this.legalCheck = document.getElementsByName("bookligitimicy");
   }
   index() {
     this.dataTable.innerHTML = "";
-
     this.libraries.forEach((item, idx) => {
       const rows = `<tr class="w-full rounded border">
         <td class="w-52 rounded-md border">${idx + 1}</td>
         <td class="w-52 rounded-md border">${item.bookName}</td>
         <td class="w-52 rounded-md border">${item.author}</td>
         <td class="w-52 rounded-md border">${item.price}</td>
-                <td class="w-52 rounded-md border">${item.type}</td>
-
+        <td class="w-52 rounded-md border">${item.type}</td>
+        <td class="w-52 rounded-md border">${item.legalCheck}</td>
         <td class="w-52 rounded-md border">
           <button class="bg-orange-500 rounded" onClick="lib.edit(${idx})">Edit</button>
           <button class="bg-red-600 rounded" onClick="lib.destroy(${idx})">Delete</button>
@@ -30,7 +30,13 @@ class Library {
     });
   }
   store() {
-    if (!this.bookName?.value || !this.author?.value || !this.price?.value) {
+    if (
+      !this.bookName?.value ||
+      !this.author?.value ||
+      !this.price?.value ||
+      !this.type?.value ||
+      !this.legalCheck?.value
+    ) {
       alert("Please fill out the form");
       return;
     }
@@ -39,6 +45,7 @@ class Library {
       author: this.author.value,
       price: this.price.value,
       type: this.type.value,
+      legalCheck: this.legalCheck.value,
     };
     if (this.update_record_index > -1) {
       this.libraries.splice(this.update_record_index, 1, data);
@@ -52,11 +59,12 @@ class Library {
   }
   edit(index) {
     this.update_record_index = index;
-    const { bookName, author, price, type } = this.libraries[index];
+    const { bookName, author, price, type, legalCheck } = this.libraries[index];
     this.bookName.value = bookName;
     this.author.value = author;
     this.price.value = price;
     this.type.value = type;
+    this.legalCheck.value = legalCheck;
     this.saveBtn.innerText = "Update";
   }
   destroy(index) {
